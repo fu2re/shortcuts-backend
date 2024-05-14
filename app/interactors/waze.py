65a70c7facdfdcd994c67b89822ceace026182cc
andrey.kozlov <andrey.kozlov@exness.com>
2024-05-14 21:15:33 +0300

    async playwright

--- app/interactors/waze.py
+++ app/interactors/waze.py
@@ -26,9 +26,12 @@ async def google_maps_link__get__coordinates(*, url):
         await expect(page.get_by_text('Before you continue')).to_have_count(1)
         await page.locator('button[aria-label="Accept all"]').first.click()
         await page.wait_for_load_state()
+        await expect(page.get_by_text('Sign in')).to_have_count(1)
+        logger.info("Terms accepted")
     except AssertionError:
         pass
 
+    await page.wait_for_url(re.compile(r".+/@.+"))
     coords = google_maps_link__parse__coordinates(full_url=page.url)
 
     await browser.close()
@@ -37,13 +40,18 @@ async def google_maps_link__get__coordinates(*, url):
 
 def google_maps_link__parse__coordinates(*, full_url):
     logger.info('Parse: %s', full_url)
+
     try:
-        lat = re.search("!3d(\-{0,1})(\d+\.\d+)", full_url).groups()[1]
-        logger.info('Lat: %s', lat)
-        lon = re.search("!4d(\-{0,1})(\d+\.\d+)", full_url).groups()[1]
-        logger.info('Lon: %s', lon)
-    except KeyError:
-        raise InvalidCoords
+        lat = re.search("!3d(\-{0,1}\d+\.\d+)", full_url).groups()[0]
+        lon = re.search("!4d(\-{0,1}\d+\.\d+)", full_url).groups()[0]
+    except (KeyError, AttributeError):
+        if match := re.search("@(\-{0,1}\d+\.\d+),(\-{0,1}\d+\.\d+)", full_url):
+            lat, lon = match.groups()
+        else:
+            raise InvalidCoords
+
+    logger.info('Lat: %s', lat)
+    logger.info('Lon: %s', lon)
     return lat, lon
 
 
